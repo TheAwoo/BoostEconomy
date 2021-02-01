789f619f9fc4b9d51300b90abfd59cf814de2b3c
ItsWagPvP <71121134+ItsWagPvP@users.noreply.github.com>
2021-02-01 09:29:49 +0100

    PlaceholderAPI and console messages

--- src/main/java/boostdevteam/boosteconomy/BoostEconomy.java
+++ src/main/java/boostdevteam/boosteconomy/BoostEconomy.java
@@ -3,6 +3,7 @@ package boostdevteam.boosteconomy;
 import boostdevteam.commands.*;
 import boostdevteam.events.PlayerJoinEvent;
 import boostdevteam.events.PluginListener;
+import boostdevteam.placeholderapi.Placeholders;
 import boostdevteam.tabcompleter.*;
 import boostdevteam.vaultapi.VEconomy;
 import boostdevteam.vaultapi.VHook;
@@ -131,7 +132,7 @@ public final class BoostEconomy extends JavaPlugin implements Listener {
             getInstance().getConfig().set("Config.UseSounds", true);
         } else {
             getInstance().getConfig().set("Config.UseSounds", false);
-            Bukkit.getConsoleSender().sendMessage("[BoostEconomy] §eThe sounds has been disabled to prevent errors, " +
+            Bukkit.getConsoleSender().sendMessage("§f-> §eThe sounds has been disabled to prevent errors, " +
                     "if you want to use the sounds you need to change that to true and set the sounds for your version!");
         }
     }
@@ -145,24 +146,53 @@ public final class BoostEconomy extends JavaPlugin implements Listener {
         if (!setupEconomy() ) {
             getServer().getPluginManager().disablePlugin(this);
         }else {
+            Bukkit.getConsoleSender().sendMessage("§8--------------------------------------");
+            Bukkit.getConsoleSender().sendMessage("            §bBoostEconomy");
+            Bukkit.getConsoleSender().sendMessage("              §aEnabling");
+            Bukkit.getConsoleSender().sendMessage("§8");
+            if(Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null){
+                if (getConfig().getBoolean("Config.Placeholders")) {
+                    try {
+                        new Placeholders(this).register();
+                    } catch (Exception e) {
+                        Bukkit.getConsoleSender().sendMessage("§f-> §cError on hooking with PlaceholderAPI");
+                    } finally {
+                        Bukkit.getConsoleSender().sendMessage("§f-> §7Hooked with §aPlaceholderAPI§7!");
+                        Bukkit.getConsoleSender().sendMessage("§f-> §7Loaded §e%boosteconomy_money% §7placeholder!");
+                    }
+                }
+            } else {
+                Bukkit.getConsoleSender().sendMessage("§f-> §7Could not find §cPlaceholderAPI§7 for placeholders, no placeholders will be added!");
+            }
+
             if (Bukkit.getVersion().contains("1.12") || Bukkit.getVersion().contains("1.13") || Bukkit.getVersion().contains("1.14") || Bukkit.getVersion().contains("1.15") || Bukkit.getVersion().contains("1.16")) {
                 Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(BoostEconomy.plugin, new Runnable() {
                     public void run() {
                         if (BoostEconomy.getInstance().getConfig().getBoolean("Config.CheckForUpdates.Console")) {
                             new UpdateChecker(plugin, 86591).getVersion(version -> {
                                 if (plugin.getDescription().getVersion().equalsIgnoreCase(version)) {
-                                    Bukkit.getConsoleSender().sendMessage("[BoostEconomy] §aNo new version available!");
+                                    Bukkit.getConsoleSender().sendMessage("§8--------------------------------------");
+                                    Bukkit.getConsoleSender().sendMessage("            §bBoostEconomy");
+                                    Bukkit.getConsoleSender().sendMessage("               §eUpdater");
+                                    Bukkit.getConsoleSender().sendMessage("§8");
+                                    Bukkit.getConsoleSender().sendMessage("§f-> §aNo new version available!");
+                                    Bukkit.getConsoleSender().sendMessage("§8--------------------------------------");
                                 } else {
-                                    Bukkit.getConsoleSender().sendMessage("[BoostEconomy] New version available! §av" + version);
-                                    Bukkit.getConsoleSender().sendMessage("[BoostEconomy] You have §cv" + plugin.getDescription().getVersion());
-                                    Bukkit.getConsoleSender().sendMessage("[BoostEconomy] §eDownload it at https://www.spigotmc.org/resources/86591");
+                                    Bukkit.getConsoleSender().sendMessage("§8--------------------------------------");
+                                    Bukkit.getConsoleSender().sendMessage("            §bBoostEconomy");
+                                    Bukkit.getConsoleSender().sendMessage("              §eUpdater");
+                                    Bukkit.getConsoleSender().sendMessage("§8");
+                                    Bukkit.getConsoleSender().sendMessage("§f-> New version available! §av" + version);
+                                    Bukkit.getConsoleSender().sendMessage("§f-> You have §cv" + plugin.getDescription().getVersion());
+                                    Bukkit.getConsoleSender().sendMessage("§f-> §eDownload it at https://www.spigotmc.org/resources/86591");
+                                    Bukkit.getConsoleSender().sendMessage("§8--------------------------------------");
                                 }
                             });
                         }
                     }
                 }, 20);
             }else {
-                Bukkit.getConsoleSender().sendMessage("[BoostEconomy] You are using a server version not compatible with the updater! §c(Works with 1.12+)");
+                Bukkit.getConsoleSender().sendMessage("§f-> You are using a server version not compatible with the updater! §c(Works with 1.12+)");
             }
 
             try {
@@ -170,10 +200,8 @@ public final class BoostEconomy extends JavaPlugin implements Listener {
                 @SuppressWarnings("unused")
                 MetricsLite metrics = new MetricsLite(this, pluginId);
             }catch (Exception e) {
-                Bukkit.getConsoleSender().sendMessage("[BoostEconomy] §cError with metrics!");
+                Bukkit.getConsoleSender().sendMessage("§f-> §cError with metrics!");
                 e.printStackTrace();
-            }finally {
-                Bukkit.getConsoleSender().sendMessage("[BoostEconomy] §aMetrics loaded with success!");
             }
             //
             try {
@@ -188,10 +216,10 @@ public final class BoostEconomy extends JavaPlugin implements Listener {
 
                     hook.onHook();
                 }catch (Exception e) {
-                    Bukkit.getConsoleSender().sendMessage("[BoostEconomy] §cError hooking with Vault!");
-                    e.printStackTrace();
+                    Bukkit.getConsoleSender().sendMessage("§f-> §cError hooking with Vault!");
+                    Bukkit.getConsoleSender().sendMessage("§f-> §cIs Vault loaded?");
                 } finally {
-                    Bukkit.getConsoleSender().sendMessage("[BoostEconomy] §aHooked successfully with Vault!");
+                    Bukkit.getConsoleSender().sendMessage("§f-> §7Hooked with §aVault§7!");
                 }
 
                 BoostEconomy.getInstance().getConfig().options().header(
@@ -204,10 +232,11 @@ public final class BoostEconomy extends JavaPlugin implements Listener {
                 loadCommands();
 
             }catch (Exception e) {
-                Bukkit.getConsoleSender().sendMessage("[BoostEconomy] §cUnexpected error!");
+                Bukkit.getConsoleSender().sendMessage("§f-> §cUnexpected error!");
                 e.printStackTrace();
             }finally {
-                Bukkit.getConsoleSender().sendMessage("[BoostEconomy] §aLoaded with success!");
+                Bukkit.getConsoleSender().sendMessage("§f-> §aPlugin loaded with success!");
+                Bukkit.getConsoleSender().sendMessage("§8--------------------------------------");
             }
         }
     }
@@ -270,16 +299,31 @@ public final class BoostEconomy extends JavaPlugin implements Listener {
     public void onDisable() {
         // Plugin shutdown logic
         if (!setupEconomy()) {
-            Bukkit.getConsoleSender().sendMessage("[BoostEconomy] §cDisabled due to no Vault dependency found!");
+            Bukkit.getConsoleSender().sendMessage("§8--------------------------------------");
+            Bukkit.getConsoleSender().sendMessage("           §bBoostEconomy");
+            Bukkit.getConsoleSender().sendMessage("             §cDisabling");
+            Bukkit.getConsoleSender().sendMessage("§8");
+            Bukkit.getConsoleSender().sendMessage("§f-> §cDisabled due to no Vault dependency found!");
+            Bukkit.getConsoleSender().sendMessage("§8--------------------------------------");
         } else {
             try {
                 hook.offHook();
             } catch (Exception e) {
-                Bukkit.getConsoleSender().sendMessage("[BoostEconomy] §cError removing hook from Vault!");
-                e.printStackTrace();
+                Bukkit.getConsoleSender().sendMessage("§8--------------------------------------");
+                Bukkit.getConsoleSender().sendMessage("           §bBoostEconomy");
+                Bukkit.getConsoleSender().sendMessage("             §cDisabling");
+                Bukkit.getConsoleSender().sendMessage("§8");
+                Bukkit.getConsoleSender().sendMessage("§f-> §cError on unhooking from Vault!");
+                Bukkit.getConsoleSender().sendMessage("§f-> §cIs Vault loaded?");
+                Bukkit.getConsoleSender().sendMessage("§8--------------------------------------");
             } finally {
-                Bukkit.getConsoleSender().sendMessage("[BoostEconomy] §aUnhooked successfully from Vault!");
-                Bukkit.getConsoleSender().sendMessage("[BoostEconomy] §cPlugin disabled with success!");
+                Bukkit.getConsoleSender().sendMessage("§8--------------------------------------");
+                Bukkit.getConsoleSender().sendMessage("           §bBoostEconomy");
+                Bukkit.getConsoleSender().sendMessage("            §cDisabling");
+                Bukkit.getConsoleSender().sendMessage("§8");
+                Bukkit.getConsoleSender().sendMessage("§f-> §7UnHooked from §aVault§7!");
+                Bukkit.getConsoleSender().sendMessage("§f-> §cPlugin disabled with success!");
+                Bukkit.getConsoleSender().sendMessage("§8--------------------------------------");
             }
         }
     }
---
+++ src/main/java/boostdevteam/placeholderapi/Placeholders.java
@@ -0,0 +1,119 @@
+package boostdevteam.placeholderapi;
+
+import boostdevteam.boosteconomy.BoostEconomy;
+import boostdevteam.misc.Economy;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.entity.Player;
+
+/**
+ * This class will be registered through the register-method in the
+ * plugins onEnable-method.
+ */
+public class Placeholders extends PlaceholderExpansion {
+
+    private BoostEconomy plugin;
+
+    /**
+     * Since we register the expansion inside our own plugin, we
+     * can simply use this method here to get an instance of our
+     * plugin.
+     *
+     * @param plugin
+     *        The instance of our plugin.
+     */
+    public Placeholders(BoostEconomy plugin){
+        this.plugin = plugin;
+    }
+
+    /**
+     * Because this is an internal class,
+     * you must override this method to let PlaceholderAPI know to not unregister your expansion class when
+     * PlaceholderAPI is reloaded
+     *
+     * @return true to persist through reloads
+     */
+    @Override
+    public boolean persist(){
+        return true;
+    }
+
+    /**
+     * Because this is a internal class, this check is not needed
+     * and we can simply return {@code true}
+     *
+     * @return Always true since it's an internal class.
+     */
+    @Override
+    public boolean canRegister(){
+        return true;
+    }
+
+    /**
+     * The name of the person who created this expansion should go here.
+     * <br>For convienience do we return the author from the plugin.yml
+     *
+     * @return The name of the author as a String.
+     */
+    @Override
+    public String getAuthor(){
+        return plugin.getDescription().getAuthors().toString();
+    }
+
+    /**
+     * The placeholder identifier should go here.
+     * <br>This is what tells PlaceholderAPI to call our onRequest
+     * method to obtain a value if a placeholder starts with our
+     * identifier.
+     * <br>The identifier has to be lowercase and can't contain _ or %
+     *
+     * @return The identifier in {@code %<identifier>_<value>%} as String.
+     */
+    @Override
+    public String getIdentifier(){
+        return "boosteconomy";
+    }
+
+    /**
+     * This is the version of the expansion.
+     * <br>You don't have to use numbers, since it is set as a String.
+     *
+     * For convienience do we return the version from the plugin.yml
+     *
+     * @return The version as a String.
+     */
+    @Override
+    public String getVersion(){
+        return plugin.getDescription().getVersion();
+    }
+
+    /**
+     * This is the method called when a placeholder with our identifier
+     * is found and needs a value.
+     * <br>We specify the value identifier in this method.
+     * <br>Since version 2.9.1 can you use OfflinePlayers in your requests.
+     *
+     * @param  player
+     *         A {@link Player Player}.
+     * @param  identifier
+     *         A String containing the identifier/value.
+     *
+     * @return possibly-null String of the requested identifier.
+     */
+
+    public String onPlaceholderRequest(Player player, String identifier){
+
+        if(player == null){
+            return "";
+        }
+
+        // %boosteconomy_money%
+        if(identifier.equals("money")){
+            Economy eco = new Economy(player, 0);
+            return "" + eco.getBalance();
+        }
+
+        // We return null if an invalid placeholder (f.e. %boosteconomy_invalidplaceholder%)
+        // was provided
+        return "Invalid placeholder!";
+    }
+}
